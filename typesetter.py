128aa990a28238a8b4c0777cc0d1d0435a0720d0
Tim Crilly <tim.crilly@boardworks.co.uk>
2013-12-11 15:35:26 +0000

    Adding absolute path to work files list

--- typesetter.py
+++ typesetter.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     typesettingDir = '/home/pi/pi-writer/typeset/'
     files = []
     for (dirpath, dirnames, filenames) in os.walk(typesettingDir):
-        files.extend(filenames)
+        files.extend(os.path.join(dirpath,filenames))
         break
     for file in files:
         typesetterThread = TypesetterThread(file)
