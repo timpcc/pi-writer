eadaa874439160eb856cdd955f1e35a2a8c25ce4
Tim Crilly <tim.crilly@boardworks.co.uk>
2013-12-12 10:13:10 +0000

    Added wait after test logger thread start

--- test.py
+++ test.py
@@ -3,6 +3,9 @@ Created on 12 Dec 2013
 
 @author: tim.crilly
 '''
+
+import time
+
 from Tests.testlogger import TestLoggerThread
 
 if __name__ == "__main__":
@@ -10,5 +13,6 @@ if __name__ == "__main__":
     testLoggerThread = TestLoggerThread()
     # Start new Network checker thread
     testLoggerThread.start("/home/pi/pi-writer/Tests/testinput1.txt")
+    time.sleep(0.1)
     testLoggerThread.join()
     print("Exiting main thread")
