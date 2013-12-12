bb932f394eaf30faf165d2394e4db6e3d1eba4d3
Tim Crilly <tim.crilly@boardworks.co.uk>
2013-12-12 10:17:16 +0000

    passing argument to test logger thread constructor rather than start as not supposed to override Thread.start

--- test.py
+++ test.py
@@ -10,9 +10,9 @@ from Tests.testlogger import TestLoggerThread
 
 if __name__ == "__main__":
     # Create new threads
-    testLoggerThread = TestLoggerThread()
+    testLoggerThread = TestLoggerThread("/home/pi/pi-writer/Tests/testinput1.txt")
     # Start new Network checker thread
-    testLoggerThread.start("/home/pi/pi-writer/Tests/testinput1.txt")
+    testLoggerThread.start()
     time.sleep(0.1)
     testLoggerThread.join()
     print("Exiting main thread")
