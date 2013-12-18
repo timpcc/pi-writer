26a54780f72ab87185441e6d9dd7396c511a651f
Tim <Tim@Desktop>
2013-12-18 20:03:33 +0000

    GPIO test

--- test.py
+++ test.py
@@ -4,15 +4,21 @@ Created on 12 Dec 2013
 @author: tim.crilly
 '''
 
-import time
+import RPi.GPIO as GPIO
+from time import sleep     # this lets us have a time delay (see line 12)
+GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering
+GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)    # set GPIO25 as input (button)
+GPIO.setup(24, GPIO.OUT)   # set GPIO24 as an output (LED)
 
-from Tests.testlogger import TestLoggerThread
+try:
+    while True:            # this will carry on until you hit CTRL+C
+        if GPIO.input(25): # if port 25 == 1
+            print "Port 25 is 1/HIGH/True - LED ON"
+            GPIO.output(24, 1)         # set port/pin value to 1/HIGH/True
+        else:
+            print "Port 25 is 0/LOW/False - LED OFF"
+            GPIO.output(24, 0)         # set port/pin value to 0/LOW/False
+        sleep(0.1)         # wait 0.1 seconds
 
-if __name__ == "__main__":
-    # Create new threads
-    testLoggerThread = TestLoggerThread("/home/pi/pi-writer/Tests/testinput1.txt")
-    # Start new Network checker thread
-    testLoggerThread.start()
-    time.sleep(0.1)
-    testLoggerThread.join()
-    print("Exiting main thread")
+finally:                   # this block will run no matter how the try block exits
+    GPIO.cleanup()         # clean up after yourself
