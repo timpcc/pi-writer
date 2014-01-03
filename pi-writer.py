43587b765e2df8512fd8cb075f723f22eb64aa5b
Tim Crilly <tim.crilly@boardworks.co.uk>
2014-01-03 10:37:05 +0000

    Making pi-writer a daemon

--- pi-writer.py
+++ pi-writer.py
@@ -8,48 +8,51 @@ from keylogger import KeyLoggerThread
 #from typesetter import TypesetterThread
 import traceback
 import logging
+import daemon
 
-exitFlag = 0
+class PiWriter(daemon.Daemon):
+    
+    def run(self):
+        logging.basicConfig(filename="pi-writer.log", level=logging.DEBUG, format="%(asctime)s;%(levelname)s;%(message)s")
+        logger = logging.getLogger(__name__)
+        # Create new threads
+        logger.debug("Creating threads...")
+        keyLoggerThread = KeyLoggerThread()
+        logger.debug("KeyLogger thread created")
+        keyLoggerThread.daemon = True
+        # start the key loggin thread
+        keyLoggerThread.start()
+        logger.debug("KeyLogger thread started")
+        try:
+            while True:
+                time.sleep(0.1)
+        except KeyboardInterrupt:
+            keyLoggerThread.stop()
+            keyLoggerThread.join()
+            logger.info("Exiting Main thread due to Keyboard interrupt.")
+            sys.exit(0)
+        except Exception as e:
+            logger.exception("Exception in main thread")
+            keyLoggerThread.stop()
+            keyLoggerThread.join()
+            logger.warn("Exiting Main thread due to exception.")
 
-logging.basicConfig(filename="pi-writer.log", level=logging.DEBUG, format="%(asctime)s;%(levelname)s;%(message)s")
-logger = logging.getLogger(__name__)
+
       
 if __name__ == "__main__":
-    # Create new threads
-    #thread1 = myThread(1, "Thread-1", 1)
-    #thread2 = myThread(2, "Thread-2", 2)
-
-    #networkCheckerThread = NetworkThread(2)
-    logger.debug("Creating threads...")
-    keyLoggerThread = KeyLoggerThread()
-    logger.debug("KeyLogger thread created")
-    # Make the network checker thread daemonic so we don't have to control it
-    #networkCheckerThread.daemon = True
-    #logger.debug("Network thread created")
-    keyLoggerThread.daemon = True
-#    typesetterThread.daemon = True
-    # Start new Network checker thread
-    #networkCheckerThread.start()
-    # start the key loggin thread
-    keyLoggerThread.start()
-    logger.debug("KeyLogger thread started")
-#    typesetterThread.start()
-    #thread2.start()
-    try:
-        while True:
-            time.sleep(0.1)
-    except KeyboardInterrupt:
-        #networkCheckerThread.stop()
-        #networkCheckerThread.join()
-        keyLoggerThread.stop()
-        keyLoggerThread.join()
-        logger.info("Exiting Main thread due to Keyboard interrupt.")
+    daemon = MyDaemon('/tmp/pi-writer-daemon-example.pid')
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        else:
+            print "Unknown command"
+            sys.exit(2)
         sys.exit(0)
-    except Exception as e:
-        logger.exception("Exception in main thread")
-        #networkCheckerThread.stop()
-        #networkCheckerThread.join()
-        keyLoggerThread.stop()
-        keyLoggerThread.join()
-        logger.warn("Exiting Main thread due to exception.")
-        sys.exit(1)
+    else:
+        print "usage: %s start|stop|restart" % sys.argv[0]
+        sys.exit(2)
+    
